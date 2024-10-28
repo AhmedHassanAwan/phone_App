ef378caf70a52c2ed407fc2bbb55c60997241c83
AhmedHasssanAwan <awanhasan2007@gmail.com>
2024-10-28 02:30:37 -0700

    Phone_App

--- cart.js
+++ cart.js
@@ -5,15 +5,18 @@ const data = localStorage.getItem("cart");
 const getItem = JSON.parse(data);
 console.log(getItem);
 
-const  div = document.querySelector("#output");
+let output = document.querySelector(".output");
 
 
 getItem.map((item) => {
     // console.log(`${item.model} ${item.brand}`)
-    div.innerHTML += `
+    output.innerHTML += `
     <div id="card1">
-    <h1>Name : ${item.brand}</h1>
-    <h1>Name : ${item.price}</h1>
+      <div class="card3-image">
+      <img src="${item.image}"/>
+      </div>
+    <h2>Name : ${item.brand}</h2>
+    <h2>Price: ${item.price}</h2>
 
 
     </div>
